4caf32b491eeb9cbc08e06d9f5d2a60e097a4549
Radhika Motisariya <109890918+rgmotisariya@users.noreply.github.com>
2025-02-10 13:25:20 +0000

    complite navbar basic structure

--- src/app/MyComponent/navbar/navbar.component.ts
+++ src/app/MyComponent/navbar/navbar.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true, 
+  imports: [CommonModule], 
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+  menuList = [
+    { id: 1, name: 'Home', link: '/' },
+    { id: 2, name: 'Product', link: '/Product' },
+    { id: 2, name: 'Service', link: '/Service' },
+    { id: 2, name: 'Service', link: '/Service' },
+    { id: 3, name: 'Gallery', link: '/Gallery' },
+    { id: 4, name: 'Contact Us', link: '/contactus' },
+  ];
 }
