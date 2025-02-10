05b16829b1de30f2041f9216deedf2938f27a8ea
Radhika Motisariya <109890918+rgmotisariya@users.noreply.github.com>
2025-02-10 11:33:44 +0000

    logo added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './MyComponent/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports:[NavbarComponent]
 })
 
 export class AppComponent {
-  name = 'Radhika';
-
-  
+  title = 'SamudraTech';
 }
