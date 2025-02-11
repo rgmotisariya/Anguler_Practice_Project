20da3292fb3b134e8b9ca9709a611e1afd459db5
Radhika Motisariya <109890918+rgmotisariya@users.noreply.github.com>
2025-02-11 13:27:47 +0000

    basic footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './MyComponent/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./MyComponent/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [NavbarComponent,RouterOutlet]
+  imports: [NavbarComponent, RouterOutlet, FooterComponent]
 })
 
 export class AppComponent {
