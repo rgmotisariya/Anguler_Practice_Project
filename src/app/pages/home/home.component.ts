51f4917bf254ceb83a8bfb87754c3f7d62c47746
Radhika Motisariya <109890918+rgmotisariya@users.noreply.github.com>
2025-02-13 13:22:09 +0000

    service continue

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
- 
+  logos: string[] = [
+    'saamarthya.png',
+    'nandini.png',
+    'mayur_dairy.png',
+    'nddb_dairy_services.png','saamarthya.png',
+    'nandini.png',
+    'mayur_dairy.png',
+
+    'nddb_dairy_services.png',
+    'saamarthya.png',
+    'nandini.png',
+    'mayur_dairy.png',
+
+    'nddb_dairy_services.png',
+    'saamarthya.png',
+    'nandini.png',
+    'mayur_dairy.png',
+
+    'nddb_dairy_services.png'
+  ];
+
+  services = [
+    {
+      title: 'Startups',
+      description: 'We deliver affordable, scalable tech solutions to support rapid startup growth and market readiness.',
+      image: 'assets/images/startup.png'
+    },
+    {
+      title: 'B2B',
+      description: 'Our solutions empower businesses with advanced technologies that improve processes, productivity, and performance.',
+      image: 'assets/images/b2b.png'
+    }]
 }
